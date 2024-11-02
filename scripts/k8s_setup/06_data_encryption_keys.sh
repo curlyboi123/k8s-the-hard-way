8a32232df12a0b4371d122dfd3dcad764c7a0071
John Colton <jcolton96@gmail.com>
2024-11-02 22:58:10 +0000

    remove whitespace

--- scripts/k8s_setup/06_data_encryption_keys.sh
+++ scripts/k8s_setup/06_data_encryption_keys.sh
@@ -22,4 +22,4 @@ export ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
 envsubst < configs/encryption-config.yaml \
   > encryption-config.yaml
 
-  scp encryption-config.yaml root@server:~/
+scp encryption-config.yaml root@server:~/
